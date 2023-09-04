fdd69f12414427ced53c2ae9e089c5332686e8da
lambeodaihiep <leduclam2001@gmail.com>
2023-09-04 21:58:18 +0700

    lambeodaihiep

---
+++ .pio/libdeps/megaatmega2560/kalman.cpp
@@ -0,0 +1,39 @@
+#ifndef KALMANCPP
+#define kalman
+
+#include "Arduino.h"
+
+class kalmanfillter
+{
+    double sigma = 0.0;
+    double mu = 198.0; // 198.0
+    double A = 1; // ma tran chuyen trang thai
+    double C = 1; // Ma tran do
+    double Q = 0.1; // nhieu qua trinh 0.001
+    double R = 3; // nhieu do 0.005
+
+  public:
+    double loc(double data)
+    {
+      double data_loc = klf(mu, sigma, data);
+      return data_loc;
+    }
+
+
+  private:
+    int klf(double mut, double sigmat, double zt)
+    {
+      // Du doan
+      double mu_dudoan = A * mut;
+      double sigma_dudoan = A * sigmat * A + Q;
+      // Hieu chinh
+      double kgain = sigma_dudoan * C / (C * sigma_dudoan * C + R);
+      double mu_hieuchinh = mu_dudoan + kgain * (zt - C * mu_dudoan);
+      double sigma_hieuchinh = (1 - kgain * C) * sigma_dudoan;
+      //
+      mu = mu_hieuchinh;
+      sigma = sigma_hieuchinh;
+      return mu_hieuchinh;
+    }
+};
+#endif
